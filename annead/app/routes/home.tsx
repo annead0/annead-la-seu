101ddaa9d9b4eeb6d4b4a770e7ce0bf7c229fbe2
annead0 <mikael12zamorano@outlook.com>
2025-01-02 20:25:12 +0000

    update #5

--- annead/app/routes/home.tsx
+++ annead/app/routes/home.tsx
@@ -2,10 +2,13 @@ import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+  return (
+    <>
+    <blockquote>Welcome to Annead's new website | ENG</blockquote>
+    <blockquote>Benvinguts al lloc web d'Annead.| CAT</blockquote>
+    <blockquote>Website in Development | El lloc web esta en desenvolupament.</blockquote>
+    </>
+  );
 }
 
 export function loader({ context }: Route.LoaderArgs) {
