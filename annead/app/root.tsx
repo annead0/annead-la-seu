b39cece65612620c2689517252181d0332350eff
annead0 <mikael12zamorano@outlook.com>
2025-01-03 12:30:07 +0000

    update #16

--- annead/app/root.tsx
+++ annead/app/root.tsx
@@ -23,6 +23,7 @@ export const links: Route.LinksFunction = () => [
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },*/
   { rel: "stylesheet", href: stylesheet },
+  {rel:"icon",type:"image/x-icon",href:"./image.png"}
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -32,7 +33,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Links/>
         <Meta/>
       </head>
       <body>
